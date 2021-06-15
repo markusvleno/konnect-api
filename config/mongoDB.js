ac48f845d439548dcaa0d4b7d286083efd3b888d
Markus Vleno <markus.vleno.personal@gmail.com>
2021-06-15 10:33:14 +0530

    15/06/21(session)

---
+++ config/mongoDB.js
@@ -0,0 +1,13 @@
+const mangoose = require("mongoose");
+
+mangoose
+    .connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then((result) => {
+        console.log("connected to db");
+    })
+    .catch((error) => {
+        console.log("connection to db failed" + error);
+    });
--- index.js
+++ index.js
@@ -1,20 +1,52 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const fs = require("fs");
 const path = require("path");
-const mangoose = require("mongoose");
+const cookie_parser = require("cookie-parser");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 const app = express();
 
-require("dotenv").config();
-const port = process.env.PORT || 3000;
+//db
+require("./config/mongoDB");
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookie_parser());
+const EXPRIRES = 1000 * 60 * 60 * 24;
 
-require("./utils/session");
+const sessionStore = new MongoDBStore({
+    uri: process.env.DB_URL,
+    collection: "sessions",
+    expires: EXPRIRES,
+    databaseName: "test",
+    connectionOptions: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+});
 
+sessionStore.on("error", function (error) {
+    console.log(error);
+});
+
+app.use(
+    session({
+        name: "SessionID",
+        secret: process.env.SECRET,
+        store: sessionStore,
+        resave: true,
+        saveUninitialized: true,
+        cookie: {
+            maxAge: EXPRIRES,
+            httpOnly: false,
+            secure: false,
+        },
+    }),
+);
 app.use(
     morgan("combined", {
         stream: fs.createWriteStream(path.join(__dirname, "log", "server.log"), { flags: "a" }),
@@ -41,22 +73,11 @@ app.use("/username", hasUsername);
 
 //entry point
 app.get("/", (req, res) => {
+    console.log(req.session);
     res.send("good to go");
 });
 
-//db connection
-mangoose
-    .connect(process.env.DB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then((result) => {
-        console.log("connected to db");
-    })
-    .catch((error) => {
-        console.log("connection to db failed" + error);
-    });
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`server is up @http://localhost:${port}`);
 });
